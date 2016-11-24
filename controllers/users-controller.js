e6ccef2a96f96b46d67ad6d9c37d2444f922340e
Zlatin Kostov <zkostov@protonmail.ch>
2016-11-24 12:05:51 +0200

    create user controller

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -1,5 +1,25 @@
 'use strict';
 
-module.exports = {
-    
+module.exports = function (data) {
+	return {
+		getAll(req, res) {
+			data.getAllUsers()
+				.then(users => {
+					// TODO:
+				});
+		},
+		getById(req, res) {
+			data.getUserById(req.params.id)
+				.then(user => {
+					// TODO:
+				});
+		},
+		create(req, res) {
+			let body = req.body;
+			data.createUser(body.user)
+				.then(() => {
+					// TODO: 
+				});
+		}
+	};
 };
