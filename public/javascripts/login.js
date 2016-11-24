e2685cb6e09973d0081aa8d0978fcc8914ed62cf
shakuu <todorov.h@gmail.com>
2016-11-24 17:49:52 +0200

    login form with crypto and updated layout

---
+++ public/javascripts/login.js
@@ -0,0 +1,30 @@
+/* globals $ CryptoJS toastr Promise*/
+
+(() => {
+  const content = $('#login-form');
+  const usernameTb = content.find('#username-tb');
+  const passwordTb = content.find('#password-tb');
+  const btnSubmit = content.find('#btn-submit');
+
+  btnSubmit.on('click', (ev) => {
+    const password = passwordTb.val();
+    const user = {
+      username: usernameTb.val(),
+      password: CryptoJS.SHA256(password).toString()
+    };
+    console.log(user);
+
+    $.ajax({
+        url: '/login',
+        method: 'POST',
+        contentType: 'application/json',
+        data: JSON.stringify(user)
+      })
+      .done((res) => {
+        toastr.success(res);
+      })
+      .fail((err) => {
+        toastr.error(err);
+      });
+  });
+})();
