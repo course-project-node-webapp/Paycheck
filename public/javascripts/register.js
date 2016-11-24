ee7060e48c021edf069f4d027bbebb57d4196d22
shakuu <todorov.h@gmail.com>
2016-11-24 22:48:02 +0200

    register.js in /public

--- public/javascripts/register.js
+++ public/javascripts/register.js
@@ -1,4 +1,26 @@
+/* globals $ */
 'use strict';
 (() => {
+  const registerForm = $('#register-form');
+  const tbUsername = registerForm.find('#tb-username');
+  const tbPassword = registerForm.find('#tb-password');
+  const tbFirstName = registerForm.find('#tb-first-name');
+  const tbLastName = registerForm.find('#tb-last-name');
+  const btnRegister = registerForm.find('#btn-register');
 
+  btnRegister.on('click', (ev) => {
+    const user = {
+      username: tbUsername.val(),
+      password: tbPassword.val(),
+      firstName: tbFirstName.val(),
+      lastName: tbLastName.val()
+    };
+
+    $.ajax({
+      url: '/register',
+      method: 'PUT',
+      contentType: 'application/json',
+      data: JSON.stringify(user)
+    });
+  });
 })();
