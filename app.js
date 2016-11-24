5d7b171c36c9c334a095518cf03b198d0f1438e4
shakuu <todorov.h@gmail.com>
2016-11-24 16:07:54 +0200

    data module

--- app.js
+++ app.js
@@ -7,10 +7,11 @@ const env = process.env.NODE_ENV || 'development';
 const config = require('./config/config')[env];
 
 const models = require('./lib/models');
+const data = require('./data');
 
 require('./config/express')(config, app);
 require('./config/database')(config);
 require('./config/passport')(models.User);
-require('./lib/routes')(app);
+require('./lib/routes')(app, data);
 
 app.listen(config.port, () => console.log(`Server running on port: ${config.port}`));
---
+++ data/index.js
@@ -0,0 +1,5 @@
+const userData = require('./user-data');
+
+module.exports = {
+  userData
+};
