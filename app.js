891e6d181302b39a2decb0c0a95879e6900b0f1d
shakuu <todorov.h@gmail.com>
2016-11-25 01:42:08 +0200

    lazy loading with require
    avoids requiring modules at startup

--- app.js
+++ app.js
@@ -3,14 +3,13 @@
 const env = process.env.NODE_ENV || 'development';
 const config = require('./config/config')[env];
 
-const controllerLoader = require('./lib/controllers')();
 const app = require('./config/express')(config);
 const models = require('./lib/models')();
 const data = require('./data')(models);
 
 require('./config/database')(config);
 require('./config/passport')(data.userData);
-require('./lib/routes')(app, controllerLoader, data);
+require('./lib/routes')(app, data);
 
 app.listen(config.port, () => {
   console.log(`Server running on port: ${config.port}`);
