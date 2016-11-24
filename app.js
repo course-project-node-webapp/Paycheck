a6ef33bf8343da71b513a703b7761d1af67266b5
shakuu <todorov.h@gmail.com>
2016-11-24 16:46:33 +0200

    removed conflict

--- app.js
+++ app.js
@@ -10,10 +10,6 @@ require('./config/database')(config);
 require('./config/passport')(models.User);
 require('./lib/routes')(app, data);
 
-<<<<<<< HEAD
-app.listen(config.port, () => console.log(`Server running on port: ${config.port}`));
-=======
 app.listen(config.port, () => {
   console.log(`Server running on port: ${config.port}`);
 });
->>>>>>> 49bda46819fe5ceb2e165762b51311ae39779fe4
