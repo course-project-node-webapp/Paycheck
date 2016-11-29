16237a0355adca50502bd474d06e76af6177c233
shakuu <todorov.h@gmail.com>
2016-11-29 09:59:40 +0200

    projectController

--- data/index.js
+++ data/index.js
@@ -3,9 +3,11 @@
 module.exports = function (models) {
   const userData = require('./user-data')(models.User);
   const organizationsData = require('./organizations-data')(models.Organization);
+  const projectsData = require('./project-data')(models.Project);
 
   return {
     userData,
-    organizationsData
+    organizationsData,
+    projectsData
   };
 };
