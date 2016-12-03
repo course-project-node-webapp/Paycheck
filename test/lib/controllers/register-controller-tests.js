71791bbfee320344a78bd5f1ccf9469da0da6ec6
shakuu <todorov.h@gmail.com>
2016-12-03 23:12:25 +0200

    registerController.register() tests

--- test/lib/controllers/register-controller-tests.js
+++ test/lib/controllers/register-controller-tests.js
@@ -2,7 +2,6 @@
 
 'use strict';
 
-const chai = require('chai');
 const sinon = require('sinon');
 
 describe('registerController', () => {
@@ -176,6 +175,256 @@ describe('registerController', () => {
       registerController.register(req, res)
         .then(() => {
           userDataMock.verify();
+        })
+        .then(done, done);
+    });
+
+    it('Should invoke userData.createUser with correct value.', (done) => {
+      const fakeUserModel = {
+        username: 'stub'
+      },
+        userData = {
+          getUserByUsername: function () {
+            return Promise.resolve(null);
+          },
+          createUser: function () {
+            return Promise.resolve(fakeUserModel);
+          }
+        },
+        logger = {},
+        req = {
+          body: {
+            username: 'stub user'
+          },
+          isAuthenticated: function () { }
+        },
+        res = {
+          status: function () { return this; },
+          render: function () { return this; },
+          redirect: function () { return this; },
+          json: function () { return this; }
+        };
+
+      const registerController = require('../../../lib/controllers/register-controller')(userData, logger);
+      const userDataMock = sinon.mock(userData);
+      userDataMock.expects('createUser')
+        .returns(Promise.resolve(fakeUserModel))
+        .withExactArgs(req.body).once();
+
+      registerController.register(req, res)
+        .then(() => {
+          userDataMock.verify();
+        })
+        .then(done, done);
+    });
+
+    it('Should invoke res.status() with value "201".', (done) => {
+      const fakeUserModel = {
+        username: 'stub'
+      },
+        userData = {
+          getUserByUsername: function () {
+            return Promise.resolve(null);
+          },
+          createUser: function () {
+            return Promise.resolve(fakeUserModel);
+          }
+        },
+        logger = {},
+        req = {
+          body: {
+            username: 'stub user'
+          },
+          isAuthenticated: function () { }
+        },
+        res = {
+          status: function () { return this; },
+          render: function () { return this; },
+          redirect: function () { return this; },
+          json: function () { return this; }
+        };
+
+      const registerController = require('../../../lib/controllers/register-controller')(userData, logger);
+      const resMock = sinon.mock(res);
+      resMock.expects('status')
+        .returnsThis()
+        .withExactArgs(201).once();
+
+      registerController.register(req, res)
+        .then(() => {
+          resMock.verify();
+        })
+        .then(done, done);
+    });
+
+    it('Should invoke res.json() with correct value.', (done) => {
+      const fakeUserModel = {
+        username: 'stub'
+      },
+        userData = {
+          getUserByUsername: function () {
+            return Promise.resolve(null);
+          },
+          createUser: function () {
+            return Promise.resolve(fakeUserModel);
+          }
+        },
+        logger = {},
+        req = {
+          body: {
+            username: 'stub user'
+          },
+          isAuthenticated: function () { }
+        },
+        res = {
+          status: function () { return this; },
+          render: function () { return this; },
+          redirect: function () { return this; },
+          json: function () { return this; }
+        };
+
+      const registerController = require('../../../lib/controllers/register-controller')(userData, logger);
+
+      const expectedValue = {
+        redirectUrl: '/profile',
+        message: `Welcome ${fakeUserModel.username}`
+      };
+
+      const resMock = sinon.mock(res);
+      resMock.expects('json')
+        .returnsThis()
+        .withExactArgs(expectedValue).once();
+
+      registerController.register(req, res)
+        .then(() => {
+          resMock.verify();
+        })
+        .then(done, done);
+    });
+
+    it('Should invoke logger.info() with correct error message when userData.getUserByUsername() return a user.', (done) => {
+      const fakeUserModel = {
+        username: 'stub'
+      },
+        userData = {
+          getUserByUsername: function () {
+            return Promise.resolve(fakeUserModel);
+          },
+          createUser: function () {
+            return Promise.resolve(fakeUserModel);
+          }
+        },
+        logger = {
+          info: function () { return this; }
+        },
+        req = {
+          body: {
+            username: 'stub user'
+          },
+          isAuthenticated: function () { }
+        },
+        res = {
+          status: function () { return this; },
+          render: function () { return this; },
+          redirect: function () { return this; },
+          json: function () { return this; }
+        };
+
+      const registerController = require('../../../lib/controllers/register-controller')(userData, logger);
+      const loggerMock = sinon.mock(logger);
+      loggerMock.expects('info')
+        .withExactArgs('Username already exists.').once();
+
+      registerController.register(req, res)
+        .then(() => {
+          loggerMock.verify();
+        })
+        .then(done, done);
+    });
+
+    it('Should invoke res.status() with value "400" when userData.getUserByUsername() finds a user.', (done) => {
+      const fakeUserModel = {
+        username: 'stub'
+      },
+        userData = {
+          getUserByUsername: function () {
+            return Promise.resolve(fakeUserModel);
+          },
+          createUser: function () {
+            return Promise.resolve(fakeUserModel);
+          }
+        },
+        logger = {
+          info: function () { }
+        },
+        req = {
+          body: {
+            username: 'stub user'
+          },
+          isAuthenticated: function () { }
+        },
+        res = {
+          status: function () { return this; },
+          render: function () { return this; },
+          redirect: function () { return this; },
+          json: function () { return this; }
+        };
+
+      const registerController = require('../../../lib/controllers/register-controller')(userData, logger);
+      const resMock = sinon.mock(res);
+      resMock.expects('status')
+        .returnsThis()
+        .withExactArgs(400).once();
+
+      registerController.register(req, res)
+        .then(() => {
+          resMock.verify();
+        })
+        .then(done, done);
+    });
+
+    it('Should invoke res.json() with correct value when userData.getUserByUsername() finds a user.', (done) => {
+      const fakeUserModel = {
+        username: 'stub'
+      },
+        userData = {
+          getUserByUsername: function () {
+            return Promise.resolve(fakeUserModel);
+          },
+          createUser: function () {
+            return Promise.resolve(fakeUserModel);
+          }
+        },
+        logger = {
+          info: function () { }
+        },
+        req = {
+          body: {
+            username: 'stub user'
+          },
+          isAuthenticated: function () { }
+        },
+        res = {
+          status: function () { return this; },
+          render: function () { return this; },
+          redirect: function () { return this; },
+          json: function () { return this; }
+        };
+
+      const registerController = require('../../../lib/controllers/register-controller')(userData, logger);
+      const resMock = sinon.mock(res);
+
+      const expectedValue = {
+        message: 'Username already exists.'
+      };
+
+      resMock.expects('json')
+        .returnsThis()
+        .withExactArgs(expectedValue).once();
+
+      registerController.register(req, res)
+        .then(() => {
+          resMock.verify();
         })
         .then(done, done);
     });
