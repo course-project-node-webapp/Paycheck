4a0893a1d02c62e0fcc4152cefb6460350a311aa
shakuu <todorov.h@gmail.com>
2016-12-03 21:22:33 +0200

    setup

---
+++ test/lib/controllers/register-controller-tests.js
@@ -0,0 +1,12 @@
+/* globals describe it*/
+
+'use strict';
+
+const chai = require('chai');
+const sinon = require('sinon');
+
+describe('registerController', () => {
+  it('should test', () => {
+    chai.expect(1).to.equal(1);
+  });
+});
