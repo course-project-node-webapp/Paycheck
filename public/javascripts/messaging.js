2960453777e6eb7bf959beecd424f3761991a038
Zlatin Kostov <zkostov@protonmail.ch>
2016-12-03 17:12:59 +0200

    merge messaging

--- public/javascripts/messaging.js
+++ public/javascripts/messaging.js
@@ -5,14 +5,15 @@
 (() => {
   const $status = $('.chat-status span'),
     $textarea = $('.chat-textarea'),
-    $messages = $('.chat-messages'),
     $messages = $('ul.messages'),
     $fullname = $('.full-name'),
     $onlineUsers = $('online-users'),
     defaultStatus = $status.text(),
+    setStatus = function(s) {
       $status.text(s);
 
       if (s !== defaultStatus) {
+        let delay = setTimeout(function() {
           setStatus(defaultStatus);
           clearInterval(delay);
         }, 3000);
@@ -21,19 +22,13 @@
 
   var socket = io.connect();
 
+  socket.on('output', function(data) {
     if (data.length) {
       for (let i = 0; i < data.length; i++) {
-        let $message = $('<div/>', {
-          'class': 'chat-message'
-        });
         let $messageWrapper = $('<li/>', { 'class': 'chat-message' });
         let $author = $(`<span class="message-author capitalize">${data[i].name}</span>`);
         let $message = $(`<span class="message">${data[i].message}</span>`);
 
-        $message.html('<b>' + data[i].name + '</b>: ' + data[i].message);
-
-        $messages.append($message);
-        $messages.insertAfter($messages);
         $messageWrapper
           .append($author)
           .append($message);
@@ -42,7 +37,6 @@
     }
   });
 
-  socket.on('status', function (data) {
   socket.on('status', function(data) {
     setStatus((typeof data === 'object') ? data.message : data);
     if (data.clear === true) {
