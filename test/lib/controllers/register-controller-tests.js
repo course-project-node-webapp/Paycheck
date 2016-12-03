a5d50c4526a8db0c0af9ac8e5a8beb2517b03c8f
shakuu <todorov.h@gmail.com>
2016-12-03 21:39:34 +0200

    registerController.index() tests

--- test/lib/controllers/register-controller-tests.js
+++ test/lib/controllers/register-controller-tests.js
@@ -6,7 +6,53 @@ const chai = require('chai');
 const sinon = require('sinon');
 
 describe('registerController', () => {
-  it('should test', () => {
-    chai.expect(1).to.equal(1);
+  describe('index()', () => {
+
+    it('Should invoke req.isAuthenticated()', () => {
+      const userData = {},
+        logger = {},
+        req = {
+          isAuthenticated: () => { }
+        },
+        res = {
+          status: function () { return this; },
+          render: function () { return this; },
+          redirect: function () { return this; }
+        };
+
+      const registerController = require('../../../lib/controllers/register-controller')(userData, logger);
+      const reqMock = sinon.mock(req);
+      reqMock.expects('isAuthenticated').once();
+
+      registerController.index(req, res);
+
+      reqMock.verify();
+    });
+
+    it('Should invoke res.Redirect() with correct url when req.isAuthenticated() returns true', () => {
+      const userData = {},
+        logger = {},
+        req = {
+          isAuthenticated: () => { }
+        },
+        res = {
+          status: function () { return this; },
+          render: function () { return this; },
+          redirect: function () { return this; }
+        };
+
+      const registerController = require('../../../lib/controllers/register-controller')(userData, logger);
+
+      sinon.stub(req, 'isAuthenticated', function () {
+        return true;
+      });
+
+      const resMock = sinon.mock(res);
+      resMock.expects('redirect').withExactArgs('/').once();
+
+      registerController.index(req, res);
+
+      resMock.verify();
+    });
   });
 });
