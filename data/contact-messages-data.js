0a43c1f37625c0169f97cab1287299d026377ec5
shakuu <todorov.h@gmail.com>
2016-12-04 18:30:46 +0200

    contact messages infrastructure

---
+++ data/contact-messages-data.js
@@ -0,0 +1,40 @@
+/* globals Promise */
+
+'use strict';
+
+module.exports = function ({models}) {
+  const {
+    ContactMessage
+  } = models;
+
+  function createContactMessage(contactMessage) {
+    const newContactMessage = ContactMessage.getContactMessage(contactMessage);
+
+    return new Promise((resolve, reject) => {
+      newContactMessage.save((err) => {
+        if (err) {
+          return reject(err);
+        }
+
+        return resolve(newContactMessage);
+      });
+    });
+  }
+
+  function findAll() {
+    return new Promise((resolve, reject) => {
+      ContactMessage.find((err, messages) => {
+        if (err) {
+          return reject(err);
+        }
+
+        return resolve(messages);
+      });
+    });
+  }
+
+  return {
+    create,
+    findAll
+  };
+};
