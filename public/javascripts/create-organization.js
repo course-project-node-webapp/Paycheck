c9f03bfa63a369b37412fb0a4709a8db4cec40b8
shakuu <todorov.h@gmail.com>
2016-12-04 15:44:49 +0200

    another AJAX route

---
+++ public/javascripts/create-organization.js
@@ -0,0 +1,45 @@
+/* globals $ Promise validator toastr window */
+
+'use strict';
+
+(() => {
+  const root = $('#create-organization');
+  const tbOrganizationName = root.find('#tb-organization-name');
+  const tbOrganizationImage = root.find('#tb-organization-image');
+  const btnSubmit = root.find('#btn-submit');
+
+  btnSubmit.on('click', () => {
+    const organization = {
+      name: tbOrganizationName.val(),
+      image: tbOrganizationImage.val()
+    };
+
+    return new Promise((resolve, reject) => {
+      if (!validator.isAlphanumeric(organization.name)) {
+        throw new Error('Invalid organization name.');
+      }
+
+      $.ajax({
+        url: '/organizations/create',
+        method: 'POST',
+        contentType: 'application/json',
+        data: JSON.stringify(organization)
+      })
+        .done((res) => {
+          resolve(res);
+        })
+        .fail((err) => {
+          reject(err);
+        });
+    })
+      .then((res) => {
+        toastr.success(res.message);
+        setTimeout(() => {
+          window.location = res.redirectUrl;
+        }, 1500);
+      })
+      .catch((err) => {
+        toastr.error(err.message);
+      });
+  });
+})();
