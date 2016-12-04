1f9053c0edc78afebd1a59a653d26a5f553c6a2c
shakuu <todorov.h@gmail.com>
2016-12-04 12:09:22 +0200

    only applying fb login when deployed

--- config/passport-facebook.js
+++ config/passport-facebook.js
@@ -3,29 +3,31 @@
 const passport = require('passport');
 const FacebookStrategy = require('passport-facebook');
 
-module.exports = function (config, userData) {
-  const facebookStrategy = new FacebookStrategy({
-    clientID: config.facebookAppId,
-    clientSecret: config.facebookAppSecret,
-    callbackURL: 'https://paycheck-app.herokuapp.com/account/login/facebook/callback'
-  },
-    function (accessToken, refreshToken, profile, next) {
-      userData.getUserByFacebookId(profile.id)
-        .then((user) => {
-          if (user) {
-            return next(null, user);
-          }
+module.exports = function (config, userData, env) {
+  if (env !== 'development') {
+    const facebookStrategy = new FacebookStrategy({
+      clientID: config.facebookAppId,
+      clientSecret: config.facebookAppSecret,
+      callbackURL: 'https://paycheck-app.herokuapp.com/account/login/facebook/callback'
+    },
+      function (accessToken, refreshToken, profile, next) {
+        userData.getUserByFacebookId(profile.id)
+          .then((user) => {
+            if (user) {
+              return next(null, user);
+            }
 
-          return userData.createUserFromFacebookUser(profile);
-        })
-        .then((user) => {
-          next(null, user);
-        })
-        .catch((err) => {
-          console.log(err);
-          next(err, false);
-        });
-    });
+            return userData.createUserFromFacebookUser(profile);
+          })
+          .then((user) => {
+            next(null, user);
+          })
+          .catch((err) => {
+            console.log(err);
+            next(err, false);
+          });
+      });
 
-  passport.use(facebookStrategy);
+    passport.use(facebookStrategy);
+  }
 };
